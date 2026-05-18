326c763b6435eacf2a934a0acd503a837d36e293
Gonzales-9E-ay2526 <gonzales.jm@obmontessori.edu.ph>
2026-05-18 16:14:37 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 function checkGrade() {
+       
     let grade = document.getElementById("grade").value;
-
-    if (grade >= 94 && grade <= 100) {
-        window.alert("Excellent");
-    }
-    else if (grade >= 87 && grade <= 93) {
-        window.alert("Above Standard");
-    }
-    else if (grade >= 80 && grade <= 86) {
-        window.alert("Standard");
-    }
-    else if (grade >= 75 && grade <= 79) {
-        window.alert("Needs Improvement");
-    }
-    else {
-        window.alert("Failed");
-    }
+    
+        if (grade >= 94 && grade <= 100) {
+            window.alert("Excellent");
+        }
+        else if (grade >= 87 && grade <= 93) {
+            window.alert("Above Standard");
+        }
+        else if (grade >= 80 && grade <= 86) {
+            window.alert("Standard");
+        }
+        else if (grade >= 75 && grade <= 79) {
+            window.alert("Needs Improvement");
+        }
+        else {
+            window.alert("Failed");
+        }
 }
